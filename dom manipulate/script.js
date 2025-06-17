c7aecfc228c8e338a7e34045d2b2b9ab1e18db0d
Ruumi9 <asepskizo27@gmail.com>
2025-06-17 15:52:40 +0700

    DOM Manipulate not end yet

---
+++ dom manipulate/script.js
@@ -0,0 +1,13 @@
+// InnerHTML
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>Rahmat</em>";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "Hello World";
+
+// element.style.<propertiesCSS>
+// const judul = document.querySelector("#judul");
+// judul.style.color = "Lightblue";
+// judul.style.backgroundColor = "salmon";
+
+// element.setAttribute
