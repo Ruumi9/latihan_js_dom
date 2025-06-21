5f78a16695ea3a55316db9b3c8080e3dfcf02269
Ruumi9 <asepskizo27@gmail.com>
2025-06-21 22:17:55 +0700

    DOM Event

---
+++ dom event/script.js
@@ -0,0 +1,23 @@
+// const p3 = document.querySelector(".p3");
+
+// function ubahWarnaP2() {
+//   p2.style.backgroundColor = "lightblue";
+// }
+// function ubahWarnaP3() {
+//   p3.style.backgroundColor = "lightblue";
+// }
+
+// const p2 = document.querySelector(".p2");
+// p2.onclick = ubahWarnaP2;
+
+// const p4 = document.querySelector("section#b p");
+// p4.addEventListener("click", () => {
+//   const ul = document.querySelector("section#b ul");
+//   const li = document.createElement("li");
+//   const textLi = document.createTextNode("Item Baru");
+//   li.appendChild(textLi);
+//   //   ul.appendChild(li);
+//   ul.append(li);
+// });
+
+const p3 = document.querySelector(".p3");
